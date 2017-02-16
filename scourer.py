1fac740b92934fd16c49647a3e387c29d3820050
MiszaZG <michalzejmo@gmail.com>
2017-02-16 12:13:55 +0100

    clean func

--- scourer.py
+++ scourer.py
@@ -14,51 +14,57 @@ else:
     from os.path import walk
 
 
-work_dir = '/home/pi/Temp/bib_test/'
+main_dir = '/home/pi/Temp/bib_test/'
 gift = '_1' # suffix for double file
+file_ext = '*' # files to change, '*' - all, '*.txt' - only txt... etc.
 
 
-def main(work_dir, gift):
-    for root, dirs, files in os.walk(work_dir):
+def clean(work_dir):
+    files_path = sorted(glob.glob(os.path.join(work_dir, file_ext)))
+    files_path = [f for f in files_path if os.path.isfile(f)]
+
+    if files_path:
+        up_basenames = [os.path.basename(f).upper() for f in files_path]
+        for i, file_path in enumerate(files_path):
+            move = False
+            ori_file_path = file_path
+            head, tail = os.path.split(file_path)
+
+            if ' ' in tail:
+                new_tail = tail.replace(' ', '_')
+                new_path = os.path.join(work_dir, new_tail)
+                files_path[i] = new_path
+                print('SPACE "{}" -> {}'.format(file_path, new_tail))
+                move = True
+                file_path = new_path
+                up_basenames[i] = new_tail.upper()
+
+                head, tail = os.path.split(file_path)
+
+            while collections.Counter(up_basenames)[tail.upper()] > 1:
+                if '.' in tail:
+                    new_tail = tail.replace('.', gift+'.')
+                else:
+                    new_tail = tail + gift
+
+                up_basenames[i] = new_tail.upper()
+                new_path = os.path.join(work_dir, new_tail)
+                files_path[i] = new_path
+                print('Double "{}" -> {}'.format(file_path, new_tail))
+                move = True
+                tail = new_tail
+                file_path = new_path
+
+            if move:
+                os.rename(ori_file_path, file_path)
+
+
+def main(main_dir, gift):
+    for root, dirs, _ in os.walk(main_dir):
+        clean(root)
         for _dir in dirs:
-            files_path = sorted(glob.glob(os.path.join(root, _dir)+'/*'))
-            files_path = [f for f in files_path if os.path.isfile(f)]
-
-            if files_path:
-                up_basenames = [os.path.basename(f).upper() for f in files_path]
-                for i, file_path in enumerate(files_path):
-                    move = False
-                    ori_file_path = file_path
-                    head, tail = os.path.split(file_path)
-
-                    if ' ' in tail:
-                        new_tail = tail.replace(' ', '_')
-                        new_path = os.path.join(root, _dir, new_tail)
-                        files_path[i] = new_path
-                        print('SPACE "{}" -> {}'.format(file_path, new_tail))
-                        move = True
-                        file_path = new_path
-                        up_basenames[i] = new_tail.upper()
-
-                    head, tail = os.path.split(file_path)
-
-                    while collections.Counter(up_basenames)[tail.upper()] > 1:
-                        if '.' in tail:
-                            new_tail = tail.replace('.', gift+'.')
-                        else:
-                            new_tail = tail + gift
-
-                        up_basenames[i] = new_tail.upper()
-                        new_path = os.path.join(root, _dir, new_tail)
-                        files_path[i] = new_path
-                        print('Double "{}" -> {}'.format(file_path, new_tail))
-                        move = True
-                        tail = new_tail
-                        file_path = new_path
-
-                    if move:
-                        os.rename(ori_file_path, file_path)
+            clean(_dir)
 
 
 if __name__ == "__main__":
-    main(work_dir, gift)
+    main(main_dir, gift)
